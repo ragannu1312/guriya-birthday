2abe3561d587fa6fe6e9e0e27ef0312b9055fa3d
ragannu1312 <ragannu1312@gmail.com>
2026-07-15 22:23:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function startSurprise() {
+
+document.querySelector(".hero").style.display="none";
+
+document.getElementById("message").style.display="block";
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+document.body.style.background="linear-gradient(135deg,#ff4b91,#ff9ed2,#fff0b3)";
+
+alert("🎉 Happy Birthday Meri Guriya ❤️");
+
+}1
